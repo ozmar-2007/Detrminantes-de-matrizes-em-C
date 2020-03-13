8514932c99b11b149aa9234b25d124c7bdccd06f
Osmar Carvalho <ozmar.2007@gmail.com>
2020-03-13 11:16:13 -0300

    iniciando a matriz chave "determinante"...

--- determinante.c
+++ determinante.c
@@ -1,1 +1,39 @@
-https://www.youtube.com/watch?v=g2Tco_v73Pc
+#include <stdio.h>
+#include <stdlib.h> 
+
+int determinante(int n, float mat[][n])
+{
+  if(n == 2)
+    return ((mat[0][0] * mat[1][1])-(mat[0][1]mat[1][0]));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//https://www.youtube.com/watch?v=g2Tco_v73Pc
